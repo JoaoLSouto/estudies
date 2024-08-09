d248c0abfdede4076e5dd3763a6dba795e9361a6
JoaoLSouto <contato.joaolsouto@gmail.com>
2024-08-09 19:58:07 -0300

    else if fixed

--- desafio.js
+++ desafio.js
@@ -1,23 +1,29 @@
 // Criar uma variável para armazenar o nome e a quantidade de experiência (XP) de um usuário. 
 // dpeois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:  
 
-let usuario = true
 let nickname = "Fulano"
-let xp = 10000
-if(xp < 1000){
-    console.log("o ranking do " + nickname + " é Ferro")
-}else if (xp >= 1000 && xp < 2000){
-    console.log("o ranking do " + nickname + " é Bronze")
-}else if (xp >= 2000 && xp < 5000){
-    console.log("o ranking do " + nickname + " é Prata")
-}else if (xp >= 5000 && xp < 4000){
-    console.log("o ranking do " + nickname + " é Ouro")
-}else if (xp >= 6000 && xp < 7000){
-    console.log("o ranking do " + nickname + " é Platina")
-}else if (xp >= 7000 && xp < 8000){
-    console.log("o ranking do " + nickname + " é Ascendente")
-}else if (xp >= 8000 && xp < 9000){
-    console.log("o ranking do " + nickname + " é Imortal")
-}else if (xp >= 9000 && xp <= 10000){
-    console.log("o ranking do " + nickname + " é Radiante")
-}
+let xp = 0
+let ranking = ""
+
+do {
+
+    if(xp < 1000){
+        ranking = "Ferro"
+    }else if (xp >= 1000 && xp < 2000){
+        ranking = "Bronze"
+    }else if (xp >= 2000 && xp < 5000){
+        ranking = "Prata"
+    }else if (xp >= 5000 && xp < 4000){
+        ranking = "Ouro"
+    }else if (xp >= 6000 && xp < 7000){
+        ranking = "Platina"
+    }else if (xp >= 7000 && xp < 8000){
+        ranking = "Ascendente"
+    }else if (xp >= 8000 && xp < 9000){
+        ranking = "Imortal"
+    }else if (xp >= 9000 && xp <= 10000){
+        ranking = "Radiante"
+    }
+	console.log("O Herói " + nickname + " está no nivel " + ranking)
+	break;
+} while (xp >= 0);
